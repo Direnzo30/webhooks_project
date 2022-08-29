6b870957268246612c9d52543d2e8d4968bfbd9f
Salvador Di Renzo <direnzosalvatore@gmail.com>
2022-08-29 18:37:26 +0200

    [FEAT-0001] adapt organizations controller for authentication

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,8 +2,16 @@
 
 # Exposes API for interacting with Organizations.
 class OrganizationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_organization
+
   def show
+    render_response(@organization, OrganizationSerializer)
+  end
+
+  private
+
+  def find_organization
     @organization = Organization.find(params[:id])
-    render json: OrganizationSerializer.new(@organization).serializable_hash
   end
 end
