f9a219e5c86ae8d8b425ab9bc9c52e64c6e64bbc
Salvador Di Renzo <direnzosalvatore@gmail.com>
2022-08-29 20:08:25 +0200

    [FEAT-0001] add concerns documentation

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
-# Module for validating
+# Module for validating the requests Authorization header
 module Authenticable
   extend ActiveSupport::Concern
 
   included do
+    # @return [User] The authenticated user
     attr_accessor :current_user
 
+    #
+    # Validates the Authorization header for granting access
+    #
+    # @return [User] The user associated to the JWT
+    #
     def authenticate_user!
       authenticate_or_request_with_http_token do |token, _|
         @current_user = Authentication::AccessService.grant_access(token)
--- app/controllers/concerns/renderable.rb
+++ app/controllers/concerns/renderable.rb
@@ -11,22 +11,58 @@ module Renderable
 
     private
 
+    #
+    # Renders a raw hash as response
+    #
+    # @param [Hash] data - The payload to be rendered
+    # @param [Symbol] status - The HTTP status related to the response
+    #
+    # @return [Hash] The serialized response
+    #
     def render_raw_response(data, status = :ok)
       render json: { data: data }, status: status
     end
 
-    def render_response(object, serializer, status = :ok)
-      render json: serializer.new(object).serializable_hash, status: status
+    #
+    # Renders a serialized model
+    #
+    # @param [Object] model - The model to be serialized
+    # @param [JSONAPI::Serializer] serializer - The serializer for serializing the model
+    # @param [Symbol] status - The HTTP status related to the response
+    #
+    # @return [Hash] The serialized model as response
+    #
+    def render_response(model, serializer, status = :ok)
+      render json: serializer.new(model).serializable_hash, status: status
     end
 
+    #
+    # Renders a not found error in case the exception is raised
+    #
+    # @return [Hash] The response
+    #
     def not_found
       render json: { error: 'Not Found' }, status: :not_found
     end
 
+    #
+    # Renders any exception raised that is not a BaseException
+    #
+    # @param [StandardError] exception - The raised exception
+    #
+    # @return [Hash] The response
+    #
     def manage_standand_error(exception)
       render json: { errors: exception.message }, status: :internal_server_error
     end
 
+    #
+    # Renders the custom exceptions defined by the app
+    #
+    # @param [BaseException] base_exception - custom exception for handling different scenarios
+    #
+    # @return [Hash] The response
+    #
     def manage_base_exception(base_exception)
       render json: { errors: base_exception.message }, status: base_exception.status_code
     end
